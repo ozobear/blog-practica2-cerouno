15ffd352ba55c4e8ce31616cb0740ea85b521616
ozobear <ozober@gmail.com>
2020-02-07 16:57:57 -0600

    falta adecuar a responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,6 @@ function Navbar() {
     return (
         <header>
             <nav className="navbar" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-            
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="/">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-                </div>
-            
                 <div id="navbarBasicExample" className="navbar-menu">
                 <div className="navbar-start">
                     <a className="navbar-item" href="/">
@@ -23,9 +14,9 @@ function Navbar() {
                     Servicios
                     </a>
 
-                    <a className="navbar-item" href="https://bulma.io">
+                    {/* <a className="navbar-item" href="https://bulma.io">
                     <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt=""></img>
-                    </a>
+                    </a> */}
             
                     <a className="navbar-item" href="/">
                     Acerca de...
